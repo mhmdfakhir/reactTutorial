1864b210722d0d8c434e69a43e4de6cc24d12484
mhmdfakhir <fakhirmohammad777@gmail.com>
2025-01-01 19:06:29 +0300

    digital clock done

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,10 +1,10 @@
 import "./App.css";
-import EffectfulComponent from "./EffectfulComponent";
+import DigitalClock from "./DigitalClock";
 
 function App() {
   return (
     <>
-      <EffectfulComponent />
+      <DigitalClock />
     </>
   );
 }
